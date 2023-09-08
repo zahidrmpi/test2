40af38e2d572d49a1a15771744664a205d8d776a
zahidrmpi <zahidhassan19@gmail.com>
2023-09-08 12:03:10 +0600

    first commit

---
+++ array.php
@@ -0,0 +1,13 @@
+<?php
+// 40 assigned to $x (assignment operator)
+$x = 40;
+
+// combined operator +=
+$x += 40; /* this means $x = $x + 40 */
+echo $x .'<br/>';
+
+//combined operator .=
+$y = 'Rajshahi';
+$y .= ' Mohila Polytechnic'; /* this means $y = $y . ' Tutorials'*/
+echo $y;
+?>
